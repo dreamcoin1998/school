b1ffa30f7af78e8373147e9f5ac4eb5a10fb9295
jingchuan998 <www.wjingchuan@163.com>
2020-04-20 03:59:43 +0800

    '20200420'

--- forum/models.py
+++ forum/models.py
@@ -3,9 +3,10 @@ from yonghu.models import Yonghu
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from Messages.models import MainMessage,Message,ReplyMessage
-
+from readAndReplyNum.getReadAndReplyNum import GetReadAndReplyNum
+from images.getlmagePath import GetImagePath
 # Create your models here.
-class Post(models.Model):
+class Post(models.Model,GetReadAndReplyNum,GetImagePath):
     '''
     发表帖子
     '''
@@ -13,14 +14,11 @@ class Post(models.Model):
     content = models.TextField(verbose_name='帖子内容')
     yonghu = models.ForeignKey(Yonghu, on_delete=models.CASCADE,verbose_name='发帖人')
     created_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
-    read_nums = models.IntegerField(default=False, verbose_name='阅读次数')
     is_deleted = models.BooleanField(default=False, verbose_name='是否删除')
-    num_views = models.IntegerField(default=False, verbose_name='浏览量')
-    @property
+
     def __str__(self):
         return self.title
 
-class MainPost(Post):
     class Meta:
         ordering = ['-created_time']
         verbose_name = '发表帖子'
