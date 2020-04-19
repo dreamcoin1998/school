e1b4e4c5fdd05cf5d3620851115d9de59143a587
jingchuan998 <www.wjingchuan@163.com>
2020-04-20 00:50:03 +0800

    'forum'

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','content','created_time','read_nums','is_deleted')
+
--- forum/models.py
+++ forum/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from yonghu.models import Yonghu
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from Messages.models import MainMessage,Message,ReplyMessage
 
 # Create your models here.
 class Post(models.Model):
+    '''
+    发表帖子
+    '''
+    title = models.CharField(max_length=30,verbose_name='帖子标题')
+    content = models.TextField(verbose_name='帖子内容')
+    yonghu = models.ForeignKey(Yonghu, on_delete=models.CASCADE,verbose_name='发帖人')
+    created_time = models.DateTimeField(auto_now_add=True,verbose_name='创建时间')
+    read_nums = models.IntegerField(default=False, verbose_name='阅读次数')
+    is_deleted = models.BooleanField(default=False, verbose_name='是否删除')
+    num_views = models.IntegerField(default=False, verbose_name='浏览量')
+    @property
+    def __str__(self):
+        return self.title
+
+class MainPost(Post):
+    class Meta:
+        ordering = ['-created_time']
+        verbose_name = '发表帖子'
+        verbose_name_plural = verbose_name
+
+class PostMessage(Message,models.Model):
+    pass
